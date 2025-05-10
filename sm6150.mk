0ef403ee1452c9489ecc194ab8e529a3a782f74e
Dhina17 <dhinalogu@gmail.com>
2025-05-10 08:32:14 +0800

    sm6150-common: Implement torch light control
    [RealJohnGalt:] modify for oneplus usage, add toggle switch
    [cyberknight777]: Adapt nodes and SEPolicy for sunny
    
    Change-Id: I8ff5d814-d58d-4ed1-9b42-b025415a5c85
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>
    Signed-off-by: eidawon <projecteidawon@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -95,6 +95,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.sm6150)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
