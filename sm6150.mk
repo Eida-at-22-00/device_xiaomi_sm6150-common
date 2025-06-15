fc80b08061aca9a4d849a8e12952cc184641adfa
eidawon <projecteidawon@gmail.com>
2025-06-15 23:44:23 +0800

    sm6150-common: Drop Lineage Health HAL
    Signed-off-by: eidawon <projecteidawon@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -224,15 +224,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-
 # Logging
 SPAMMY_LOG_TAGS := \
     AiAiEcho \
